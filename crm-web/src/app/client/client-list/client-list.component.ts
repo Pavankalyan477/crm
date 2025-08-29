acc99f5d65997036afbc67d6b7346f4f9fbc0ab3
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2025-08-29 12:02:32 +0530

    All Screens UI Tables With dummy data

--- crm-web/src/app/client/client-list/client-list.component.ts
+++ crm-web/src/app/client/client-list/client-list.component.ts
@@ -7,19 +7,20 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatExpansionModule, MatInputModule, MatFormFieldModule,MatPaginatorModule, MatTableModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatExpansionModule, MatInputModule, MatFormFieldModule,MatPaginatorModule, MatTableModule, MatIconModule],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.css'
 })
 export class ClientListComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['position', 'name', 'mobile', 'email', 'actions'];
+  dataSource = new MatTableDataSource<Client>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -28,33 +29,33 @@ export class ClientListComponent {
   }
 }
 
-export interface PeriodicElement {
+export interface Client {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  mobile: number;
+  email: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const ELEMENT_DATA: Client[] = [
+  {position: 1, name: 'Hydrogen', mobile: 1.0079, email: 'H'},
+  {position: 2, name: 'Helium', mobile: 4.0026, email: 'He'},
+  {position: 3, name: 'Lithium', mobile: 6.941, email: 'Li'},
+  {position: 4, name: 'Beryllium', mobile: 9.0122, email: 'Be'},
+  {position: 5, name: 'Boron', mobile: 10.811, email: 'B'},
+  {position: 6, name: 'Carbon', mobile: 12.0107, email: 'C'},
+  {position: 7, name: 'Nitrogen', mobile: 14.0067, email: 'N'},
+  {position: 8, name: 'Oxygen', mobile: 15.9994, email: 'O'},
+  {position: 9, name: 'Fluorine', mobile: 18.9984, email: 'F'},
+  {position: 10, name: 'Neon', mobile: 20.1797, email: 'Ne'},
+  {position: 11, name: 'Sodium', mobile: 22.9897, email: 'Na'},
+  {position: 12, name: 'Magnesium', mobile: 24.305, email: 'Mg'},
+  {position: 13, name: 'Aluminum', mobile: 26.9815, email: 'Al'},
+  {position: 14, name: 'Silicon', mobile: 28.0855, email: 'Si'},
+  {position: 15, name: 'Phosphorus', mobile: 30.9738, email: 'P'},
+  {position: 16, name: 'Sulfur', mobile: 32.065, email: 'S'},
+  {position: 17, name: 'Chlorine', mobile: 35.453, email: 'Cl'},
+  {position: 18, name: 'Argon', mobile: 39.948, email: 'Ar'},
+  {position: 19, name: 'Potassium', mobile: 39.0983, email: 'K'},
+  {position: 20, name: 'Calcium', mobile: 40.078, email: 'Ca'},
 ];
 
--- crm-web/src/app/roles/roles-list/roles-list.component.ts
+++ crm-web/src/app/roles/roles-list/roles-list.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-roles-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatExpansionModule, MatInputModule, MatFormFieldModule, MatPaginatorModule, MatTableModule, MatIconModule],
   templateUrl: './roles-list.component.html',
   styleUrl: './roles-list.component.css'
 })
 export class RolesListComponent {
-
+  displayedColumns: string[] = ['id', 'role', 'actions'];
+    dataSource = new MatTableDataSource<Role>(ELEMENT_DATA);
+  
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+    ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+    }
 }
+
+export interface Role {
+  role: string;
+  id: number;
+}
+
+const ELEMENT_DATA: Role[] = [
+  {id: 1, role: 'Hydrogen'},
+  {id: 2, role: 'Helium'},
+  {id: 3, role: 'Lithium'},
+  {id: 4, role: 'Beryllium'},
+  {id: 5, role: 'Boron'},
+  {id: 6, role: 'Carbon'},
+  {id: 7, role: 'Nitrogen'},
+  {id: 8, role: 'Oxygen'},
+  {id: 9, role: 'Fluorine'},
+  {id: 10, role: 'Neon'},
+  {id: 11, role: 'Sodium'},
+  {id: 12, role: 'Magnesium'},
+  {id: 13, role: 'Aluminum'},
+  {id: 14, role: 'Silicon'},
+  {id: 15, role: 'Phosphorus'},
+  {id: 16, role: 'Sulfur'},
+  {id: 17, role: 'Chlorine'},
+  {id: 18, role: 'Argon'},
+  {id: 19, role: 'Potassium'},
+  {id: 20, role: 'Calcium'},
+];
